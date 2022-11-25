0af9e37cb87dc752eb0e7d5a5f9b349cc0d6cd79
Md Mahafujur Rahaman Masud <masudhossainmbs129@gmail.com>
2022-11-25 20:45:41 +0600

    all buyer component complete

--- src/Components/Pages/Dashboard/AdminOnly/AddBuyers.js
+++ src/Components/Pages/Dashboard/AdminOnly/AddBuyers.js
@@ -1,11 +1,91 @@
 import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import Swal from "sweetalert2";
 
 const AddBuyers = () => {
-    return (
+    const {
+        data: allBuyer = [],
+        isLoading,
+        refetch,
+      } = useQuery({
+        queryKey: ["allUser"],
+        queryFn: () =>
+          fetch("http://localhost:5000/allUser").then((res) => res.json()),
+      });
+    
+      const handleDelete = (id) => {
+        Swal.fire({
+          title: "Are you sure?",
+          text: "You want to delete this Buyer",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, delete him!",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            fetch(`http://localhost:5000/allSeller/${id}`, {
+              method: "DELETE",
+            })
+              .then((res) => res.json())
+              .then((data) => {
+                refetch();
+                Swal.fire("Deleted!", "Your User has been deleted.", "success");
+              });
+          }
+        });
+      };
+    
+      return (
         <div>
-            <h1>All buyers</h1>
+          <div className="my-8">
+            <h1 className="text-4xl font-serif font-bold text-center">
+              All Buyer
+            </h1>
+          </div>
+          {allBuyer?.length > 0 ? (
+            <div className="overflow-x-auto">
+              <table className="table table-zebra w-full">
+                {/*  <!-- head --> */}
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Role</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {allBuyer?.map((buyer, i) => (
+                    <tr key={buyer._id} className="hover">
+                      <th>{i + 1}</th>
+                      <td>{buyer.name}</td>
+                      <td>{buyer.email}</td>
+                      <td>{buyer.role}</td>
+                      <td>
+                        <button
+                          onClick={() => handleDelete(buyer._id)}
+                          className="btn btn-sm bg-black"
+                        >
+                          Remove Buyer
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          ) : (
+            <div>
+              <h1 className="text-4xl font-bold font-serif text-red-500">
+                {" "}
+                No Buyer Found
+              </h1>
+            </div>
+          )}
         </div>
-    );
+      );
 };
 
 export default AddBuyers;
